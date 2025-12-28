729e0973314f4956fa1872fb7f5e272e38662232
sergeiincor-png <sergei.incor@gmail.com>
2025-12-28 14:01:16 +0300

    Update database.py

--- api/models/database.py
+++ api/models/database.py
@@ -1,1 +1,14 @@
-~º&²©Z•È^›Æ³)ÜŠˆ¦¦Šír·šµ
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL)
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
+    async with async_session() as session:
+        yield session
