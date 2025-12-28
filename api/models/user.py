9b64d9473d86b391453c284de24f256752110d8f
sergeiincor-png <sergei.incor@gmail.com>
2025-12-28 14:01:38 +0300

    Update user.py

--- api/models/user.py
+++ api/models/user.py
@@ -1,1 +1,11 @@
-~º&²©Z•È^›
+from sqlalchemy import BigInteger, String, DateTime, Column
+from sqlalchemy.sql import func
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    telegram_id = Column(BigInteger, primary_key=True, index=True)
+    username = Column(String, nullable=True)
+    first_name = Column(String, nullable=False)
+    photo_url = Column(String, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
