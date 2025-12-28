ba3925fbccf279d0552ace482495ea34b5adb94b
sergeiincor-png <sergei.incor@gmail.com>
2025-12-28 14:00:18 +0300

    Update config.py

--- api/core/config.py
+++ api/core/config.py
@@ -1,1 +1,9 @@
-Šjh®Ú,~º&§'ZžØœþÇ­¶)à²)©¢»AjÇ
+import os
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "")
+    JWT_SECRET: str = os.getenv("JWT_SECRET", "super-secret-key-123")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+
+settings = Settings()
